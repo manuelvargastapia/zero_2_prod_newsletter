901f31c91bfda9d10df2700ed503795cf491c165
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-27 14:41:32 -0300

    create subscribe() integrations tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -39,3 +39,58 @@ fn spawn_app() -> String {
 
     format!("http://127.0.0.1:{}", port)
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+// This is an example of table-driven test (aka parametrised test). It is particularly
+// helpful when dealing with bad inputs - instead of duplicating test logic several
+// times we can simply run the same assertion against a collection of known invalid
+// bodies that we expect to fail in the same way
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+                400,
+                response.status().as_u16(),
+                "The API did not fail with 400 Bad Request when the payload was {}.",
+                error_message
+            );
+    }
+}
