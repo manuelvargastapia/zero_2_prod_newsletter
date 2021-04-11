000295c7ded06658aed05e9e226c608752185c8e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-11 12:19:23 -0400

    compose app address based on env-aware hierarchical configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,8 @@
+use std::{
+    convert::{TryFrom, TryInto},
+    env::current_dir,
+};
+
 /// Struct that models our app-level configurations.
 ///
 /// We have two grous of configuration to handle: `actix-web` server
@@ -6,7 +11,22 @@
 #[derive(serde::Deserialize)]
 pub struct Configurations {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationConfigurations,
+}
+
+/// Configurable portion of the running application address.
+///
+/// We need a _hierarchical configuration_ to make portions of the application
+/// address configurable depending on the environment. The project has a _base_
+/// configuration file that keeps the values shared accross local and production
+/// environment. Then, it has a collection of environment-specific files specifying
+/// values for fields that require customisation. Finally, the configurations
+/// depends on an environment variables, APP_ENVIRONMENT to determine the running
+/// environment.
+#[derive(serde::Deserialize)]
+pub struct ApplicationConfigurations {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -40,15 +60,64 @@ impl DatabaseSettings {
     }
 }
 
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Produduction,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Produduction => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Produduction),
+            other => Err(format!(
+                "{} is not a supported environment. Use either 'local' or 'production'.",
+                other
+            )),
+        }
+    }
+}
+
 /// Read configurations from top-level file
 pub fn get_configurations() -> Result<Configurations, config::ConfigError> {
     // Initialize configuration reader
     let mut configurations = config::Config::default();
 
-    // Add configuration values from a file named `configuration`.
-    // It will look for any top-level file with an extension
-    // that `config` knows how to parse: yaml, json, etc.
-    configurations.merge(config::File::with_name("configurations"))?;
+    // Compose the "default" configurations path
+    let base_path = current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configurations");
+
+    // Read the "default" configuration file
+    configurations
+        .merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
+    // Detect the running environment.
+    // Default to local if unspecified
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    // Layer on the environment-specific values
+    configurations.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
+    // Add in configurations from environment variables (with a prefix of APP and "__" as
+    // separator). E. g., "APP_APPLICATION__PORT=5001" would set Configurations.application.port
+    configurations.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     // Try to convert the configuration values it read into
     // our Configurations type
